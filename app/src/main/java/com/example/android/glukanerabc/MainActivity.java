23e5e120e2e464ba705e5ca87784fa0dc5824eb7
Konstantinos Tsekos <konstantinos.tsekos@gmail.com>
2018-02-17 17:22:29 +0200

    Added all intents, minor details (map, drawables) to finish

--- app/src/main/java/com/example/android/glukanerabc/MainActivity.java
+++ app/src/main/java/com/example/android/glukanerabc/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.glukanerabc;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void showMap(View view) {
+        Uri geolocation = Uri.parse("geo:47.6,-122.3?z=11");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+        mapIntent.setData(geolocation);
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+    }
+
+    public void callTeam(View view) {
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse("tel:" + "2106659220"));
+        if (callIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(callIntent);
+        }
+    }
+
+    public void viewWebpage(View view) {
+        Uri webpage = Uri.parse("http://www.gsgn.gr");
+        Intent websiteIntent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (websiteIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(websiteIntent);
+        }
+    }
+
+    public void composeEmail(View view) {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        emailIntent.setData(Uri.parse("mailto: gysy.gln@gmail.com"));
+        startActivity(Intent.createChooser(emailIntent, ""));
+        if (emailIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(emailIntent);
+        }
+    }
+
 }
